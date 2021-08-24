054e755d45bd50a5d1d127efce4fe28e7f95d9b3
Sehoon <sizzsaint@gamil.com>
2021-08-24 12:55:32 +0900

    이거해보실분

---
+++ src/main/java/com/movieingwalk/www/HomeController.java
@@ -0,0 +1,18 @@
+package com.movieingwalk.www;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+	@RequestMapping("/hello.com")
+	public String index(Model model) {
+		model.addAttribute("message", "좋은 아침");
+		model.addAttribute("now", new Date());
+		return "index";
+	}
+}
