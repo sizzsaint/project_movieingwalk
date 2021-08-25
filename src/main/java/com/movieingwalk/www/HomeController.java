f7fdda19db136d7999ce57371a4dee16be3ec715
josoyoung <34125379+josoyoung@users.noreply.github.com>
2021-08-26 08:54:46 +0900

    no message

--- src/main/java/com/movieingwalk/www/HomeController.java
+++ src/main/java/com/movieingwalk/www/HomeController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/com")
+	@RequestMapping("/")
 	public String index(Model model) {
 		model.addAttribute("message", "좋은 아침");
 		model.addAttribute("now", new Date());
-		return "index";
+		return "main/index";
 	}
 }
