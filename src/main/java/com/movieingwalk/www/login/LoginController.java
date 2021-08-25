ec744c8b577b757db46803909ace357f5750656d
Sehoon <sizzsaint@gamil.com>
2021-08-25 21:43:55 +0900

    회원가입 뷰만 하면될듯

--- src/main/java/com/movieingwalk/www/login/LoginController.java
+++ src/main/java/com/movieingwalk/www/login/LoginController.java
@@ -2,16 +2,32 @@ package com.movieingwalk.www.login;
 
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+
+import com.movieingwalk.www.bean.MemberBean;
 @Controller
 public class LoginController {
 	
-	@RequestMapping("/register")
-	public String index(Model model) {
-
+	private static final Logger logger = 
+			LoggerFactory.getLogger(LoginController.class);
+	@Autowired
+	LoginService loginservice;
+	
+	@RequestMapping(value="/registermember", method = RequestMethod.POST)
+	public String index(Model model, MemberBean memberBean) {
+		
+			logger.debug("registerMember called!!!");
+			loginservice.registerMember(memberBean);
+			model.addAttribute("result",1); //뷰 폼값 자동 매핑
+			
+		
 		return "login/register";
 	}
 
--- src/main/java/com/movieingwalk/www/login/LoginMapper.java
+++ src/main/java/com/movieingwalk/www/login/LoginMapper.java
@@ -13,11 +13,20 @@ public interface LoginMapper {
 	String u_id, u_password, u_name, u_email, u_phone, u_date, u_sex, u_admin;
  * 
  */
-		final String REGISTER = " INSERT INTO MEMBER(u_idx,,u_id,u_password,u_name,u_phone,u_sex,u_email,u_age) values()";
-				
-
+		final String REGISTER = " INSERT INTO MEMBER(u_idx,,u_id,u_password,u_name,u_phone,u_sex,u_email,u_age)"
+				+ " values(${u_idx},${u_id},${u_password},${u_name},${u_phone},${u_sex},${u_email},${u_age})";
 	@Insert(REGISTER)
-	void modifyMember(MemberBean memberBean);
+	void registerMember(MemberBean memberBean);
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
