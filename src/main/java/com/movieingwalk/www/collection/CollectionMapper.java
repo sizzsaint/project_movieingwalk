1111a85c3b2cc4f3a168db3b897b8f753a9412fd
Sehoon <sizzsaint@gamil.com>
2021-09-09 10:38:09 +0900

    컬렉션메인

--- src/main/java/com/movieingwalk/www/collection/CollectionMapper.java
+++ src/main/java/com/movieingwalk/www/collection/CollectionMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
@@ -68,5 +69,35 @@ public interface CollectionMapper {
 	ArrayList<CollectionBean> getCollectionList(int m_idx);
 	
 	
+	final String SELECT_COL_LIKED = "select * from collection order by col_likes desc";
+	@Select(SELECT_COL_LIKED)
+	@Results(id="mainResult", value= {
+			@Result(property="col_idx",column="col_idx"),
+			@Result(property="u_id",column="u_id"),
+			@Result(property="col_title",column="col_title"),
+			@Result(property="col_memo",column="col_memo"),
+			@Result(property="col_midx1",column="col_movie1_idx"),
+			@Result(property="col_midx2",column="col_movie2_idx"),
+			@Result(property="col_midx3",column="col_movie3_idx"),
+			@Result(property="col_midx4",column="col_movie4_idx"),
+			@Result(property="col_midx5",column="col_movie5_idx"),
+			@Result(property="col_midx6",column="col_movie6_idx"),
+			@Result(property="col_midx7",column="col_movie7_idx"),
+			@Result(property="col_midx8",column="col_movie8_idx"),
+			@Result(property="col_midx9",column="col_movie9_idx"),
+			@Result(property="col_midx10",column="col_movie10_idx")
+	})
+	ArrayList<CollectionBean> getMainCollection();
 	
-}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+			}
